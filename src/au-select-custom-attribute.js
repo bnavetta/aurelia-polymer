98aed3f0064af3e911d9264d92bd23dd8e529b58
hirano-s <hirano-s@ppa.social>
2016-08-20 13:25:44 +0900

    added au-select attribute.

---
+++ src/au-select-custom-attribute.js
@@ -0,0 +1,12 @@
+import {customAttribute, inject} from 'aurelia-framework';
+
+export class AuSelectCustomAttribute {
+	constructor(element) {
+		element._updateItems = function() {
+		    //var nodes = window.Polymer.dom(this).queryDistributedElements(this.selectable || '*');
+		    var nodes = window.Polymer.dom(this).querySelectorAll(this.selectable || '*');
+		    nodes = Array.prototype.filter.call(nodes, this._bindFilterItem);
+		    this._setItems(nodes);
+		}
+	}	
+}
